faa4c44e24280dccf3a46544c9bd0d322aa6a230
damian alvarez <gusdam.alvarez@gmail.com>
2023-09-12 12:41:55 -0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import { NavBar } from './components/NavBar'
+
 import { ItemListContainer } from './components/ItemListContainer'
+import { ItemDetailConteiner } from './components/ItemDetailConteiner'
 function App() {
   
 
   return (
-    <>
+    <BrowserRouter>
       <NavBar/>
-      <ItemListContainer greeting={"Bienvenido a la tienda de Mates R&C"}/>
-    </>
+      
+      <Routes>
+        {/* <ItemListContainer/> */}
+        <Route exact path='/' element={<ItemListContainer/>}/>
+        <Route exact path='/category/:id' element={<ItemListContainer/>}/>
+        <Route exact path='/item/:id' element={<ItemDetailConteiner/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
